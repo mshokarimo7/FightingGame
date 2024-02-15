9b603ef38489c39ac80c69b3f67119d01f85069b
mshokarimo7 <mshokarim@gmail.com>
2024-02-15 18:34:13 +0500

    Game timers and winner determination

--- index.js
+++ index.js
@@ -123,6 +123,36 @@ function rectangularCollision({rectangle1, rectangle2}){
     )
 }
 
+function determineWinner({player, enemy, timerId}){
+    clearTimeout(timerId)
+    document.querySelector("#displayText").style.display = "flex"
+    if(player.health === enemy.health){
+        document.querySelector("#displayText").innerHTML = "Tie"
+    }
+    else if(player.health > enemy.health ){
+        document.querySelector("#displayText").innerHTML = "Player wins!"
+    }
+    else if(enemy.health > player.health){
+        document.querySelector("#displayText").innerHTML = "Enemy wins!"
+    }
+}
+
+let timer = 30
+let timerId
+
+function decreaseTimer(){
+    if(timer > 0){
+        timerId = setTimeout(decreaseTimer, 1000)
+        document.querySelector("#timer").innerHTML = timer
+        timer--
+    }
+    if(timer === 0){
+        determineWinner({player: player, enemy: enemy, timerId: timerId})
+    }
+}
+
+decreaseTimer()
+
 function animate(){
     window.requestAnimationFrame(animate)
     c.fillStyle = "black"
@@ -162,6 +192,11 @@ function animate(){
             enemy.isAttacking = false
             player.health -= 20
             document.querySelector("#playerHealth").style.width = player.health + "%"
+    }
+
+    // end game based on health 
+    if(enemy.health <= 0 || player.health <= 0){
+        determineWinner({player: player, enemy: enemy, timerId: timerId})
     }
 }
 
